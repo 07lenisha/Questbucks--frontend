bf97f7a9d02b719dfbfa514159126648c8058436
LENISHA FARASK MENDONSA <104376934+07lenisha@users.noreply.github.com>
2025-06-21 17:32:41 +0530

    Update axios.js

--- src/Config/axios.js
+++ src/Config/axios.js
@@ -1,4 +1,7 @@
-import axios  from "axios";
-export default axios.create({
-    baseURL:"http://localhost:3077"
-})
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default API;
